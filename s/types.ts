f34dc2512813959ab1cb47c359f37ba54c30d4ea
zenkyuv <przemekgg2002@gmail.com>
2022-12-07 22:50:03 +0100

    add: new types

--- s/types.ts
+++ s/types.ts
@@ -1,7 +1,33 @@
 
-import {MulePack} from "./elements/mule-pack/element2.js"
+import {MulePack} from "./elements/mule-pack/element.js"
+
+type Item = any
 
 export interface PackAddress {
 	pack: MulePack
 	index: number
 }
+
+export interface BoxGridProps {
+	boxes: Box[]
+	drag: {
+		index: number
+		box: Box
+	} | undefined
+	size: string
+	tradeHandlers: {
+		onTradeStart(sourceIndex: number): void
+		onTradeCommit(targetIndex: number): void
+	}
+}
+
+export interface Box {
+	item: Item
+	quantity: number
+}
+
+export interface Drag {
+	index: number
+	box: Box
+}
+
