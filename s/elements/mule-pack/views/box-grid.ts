977b7cc0dc567e86de05616f1245deefb5867f1f
zenkyuv <przemekgg2002@gmail.com>
2022-12-07 22:37:23 +0100

    add: box grid view

---
+++ s/elements/mule-pack/views/box-grid.ts
@@ -0,0 +1,33 @@
+
+import {html} from "lit"
+import {view} from "@chasemoskal/magical/x/view/view.js"
+import {Box} from "../element2.js"
+import {getPackSize} from "../utils/get-pack-size.js"
+import {BoxGridProps} from "../../../types.js"
+
+export const BoxGrid = view(use => (
+	{boxes,
+		drag,
+		size,
+	tradeHandlers}: BoxGridProps) => {
+
+	const gridSize = getPackSize(size)
+
+	return html`
+		<div style=${`
+			grid-template-columns: repeat(${gridSize.columns}, auto);
+			grid-template-rows: repeat(${gridSize.rows}, auto);
+			`} class="grid items">
+			${boxes.map(({item}, i) => html`<div class=item-box>
+				<div draggable=${item ? true : false}
+				class=item
+				@dragstart=${() => tradeHandlers.onTradeStart(i)}
+				@drop=${() => tradeHandlers.onTradeCommit(i)}
+				@dragover=${(e: DragEvent) => e.preventDefault()}
+				>
+				${item}
+			</div>
+			</div>`)}
+		</div>
+	`
+})
