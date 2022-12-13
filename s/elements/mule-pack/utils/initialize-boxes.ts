8a17b3f678b0bd98db9d96c7cca6dceb9931b15f
zenkyuv <przemekgg2002@gmail.com>
2022-12-13 02:06:41 +0100

    update: initialize boxes method

--- s/elements/mule-pack/utils/initialize-boxes.ts
+++ s/elements/mule-pack/utils/initialize-boxes.ts
@@ -1,10 +1,10 @@
 import {Box} from "../../../types.js"
 import shieldSvg from "../../shield.svg.js"
-import swordSvg from "../../sword.svg.js"
+import swordSvg from "../../demo.svg.js"
 import {getPackSize} from "./get-pack-size.js"
 
 export function initializeBoxes(size: string) {
 	const packSize = getPackSize(size)
-	const arr: Box[] = new Array(packSize.columns * packSize.rows).fill({item: undefined, quantity: 0}).fill({item: Math.random() < 0.5 ? swordSvg : shieldSvg, quantity: 1}, 0, 1)
+	const arr: Box[] = new Array(packSize.columns * packSize.rows).fill({item: undefined, quantity: 0}).fill({item: Math.random() < 0.5 ? swordSvg : shieldSvg, quantity: 1}, Math.random() < 0.5 ? 0 : 1, 2)
 	return arr
 }
